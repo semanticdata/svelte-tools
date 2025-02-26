618fc159b6f7c84177185d7d9a0e92d22773653c
semanticdata <contact@miguelpimentel.do>
2025-02-26 13:43:25 -0600

    fix tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [svelte(), tailwind()]
 });
